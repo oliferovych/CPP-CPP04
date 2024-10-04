0352cb321c2fc4dafbf1961fb26ef0d7e7d298f5
oliferovych <dimasikolif@gmail.com>
2024-10-04 14:33:17 +0200

    +tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,34 +6,49 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:17:07 by dolifero          #+#    #+#             */
-/*   Updated: 2024/09/27 17:05:20 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:30:53 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongCat.hpp"
 
 int main()
 {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	const WrongAnimal* wrongMeta = new WrongAnimal();
-	const WrongAnimal* wi = new WrongCat();
+	std::cout << "Test1: Creating Jack and Bob:\n" << std::endl;
+	Animal* Jack = new Dog();
+	Animal* Bob = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	std::cout << wi->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	wi->makeSound();
-	meta->makeSound(); //trying to call a virtual function from a non-instantiated class
-	wrongMeta->makeSound(); //trying to call a virtual function from a non-instantiated class
-	delete meta;
-	delete j;
-	delete i;
-	delete wrongMeta;
-	delete wi;
+	std::cout << "Jack is a " << Jack->getType() << std::endl;
+	std::cout << "Bob is a " << Bob->getType() << std::endl;
+	delete Jack;
+	delete Bob;
+	std::cout << "\nTest2: Creating 3 cats and 3 dogs:\n" << std::endl;
+	Animal *animals[6];
+	for (int i = 0; i < 6; i++)
+	{
+		if (i % 2 == 0)
+			animals[i] = new Dog();
+		else
+			animals[i] = new Cat();
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		animals[i]->makeSound();
+		delete animals[i];
+	}
+	std::cout << "\nTest3: Creating a copy of a dog with the same brain\n" << std::endl;
+	Dog *dog = new Dog();
+	std::cout << "Dog's idea0: " << dog->get_idea(0) << std::endl;
+	dog->set_idea("I like to bark", 1);
+	std::cout << "Dog's idea1: " << dog->get_idea(1) << std::endl;
+	dog->set_idea("I like to eat", 2);
+	std::cout << "Dog's idea2: " << dog->get_idea(2) << std::endl;
+	Dog *dog2 = new Dog(*dog);
+	std::cout << "Dog2's idea0: " << dog2->get_idea(0) << std::endl;
+	std::cout << "Dog2's idea1: " << dog2->get_idea(1) << std::endl;
+	std::cout << "Dog2's idea2: " << dog2->get_idea(2) << std::endl;
+	delete dog;
+	delete dog2;
 	return (0);
 }
